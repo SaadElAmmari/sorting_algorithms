98152d4c94943d6c696e5b14cf0151be19e1b13d
SaadElAmmari <giochi6@hotmail.com>
2024-10-28 06:18:26 -0700

    please work

---
+++ 0-bubble_sort.c
@@ -0,0 +1,28 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - sort list with bubble
+ * @array: The array to be printed
+ * @size: Number of elements in @array
+ */
+void bubble_sort(int *array, size_t size)
+{
+	size_t i = 0, o = size;
+	int temp;
+
+	if (array == NULL)
+		return;
+	for (o = size; o > 0; o--)
+	{
+		for (i = 0; i < size - 1; i++)
+		{
+			if (array[i] > array[i + 1])
+			{
+				temp = array[i];
+				array[i] = array[i + 1];
+				array[i + 1] = temp;
+				print_array(array, size);
+			}
+		}
+	}
+}
